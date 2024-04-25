b4d6a340db0ac95a6c36fe01c150fae24e4d0fe0
Minh Thao <63253660+minhthao56@users.noreply.github.com>
2024-04-26 01:00:30 +0700

    Add dependencies and setup database connection for problem 5

--- src/problem5/src/routers/resource.ts
+++ src/problem5/src/routers/resource.ts
@@ -1,7 +1,12 @@
 import express, { Router } from "express";
 
+import { users } from '../schemas/user'
+import { db } from "../db/connection";
+
 const router: Router = express.Router();
-router.get("/", (req, resp)=>{
+router.get("/", async (req, resp)=>{
+    const allUsers = await db.select().from(users);
+    console.log(allUsers);
     resp.send("Hello World");
 });
 
