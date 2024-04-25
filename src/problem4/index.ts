11d89ba8d28b84d98555fd0adcd896f388c1fc5b
Minh Thao <63253660+minhthao56@users.noreply.github.com>
2024-04-26 00:05:12 +0700

    Add big)

--- src/problem4/index.ts
+++ src/problem4/index.ts
@@ -1,17 +1,19 @@
 function sum_to_n_a(n: number): number {
+    // complexity: O(n)
     let sum = 0;
     for (let i = 1; i <= n; i++) {
         sum += i;
     }
     return sum;
-	
 }
 
 function sum_to_n_b(n: number): number {
+    // complexity: O(1)
     return ((1 + n) * n) / 2;
 }
 
 function sum_to_n_c(n: number): number {
+    // complexity: O(n)
     if (n <= 0) {
         return 0;
     }
